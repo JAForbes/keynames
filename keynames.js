ce332a675ea9cb163305b577952c6bc337fbdb40
James Forbes <james.a.forbes@gmail.com>
2015-04-03 14:04:46 +1100

    Create keynames.js

---
+++ keynames.js
@@ -0,0 +1,71 @@
+keyNames = function( e ){
+
+			// Listen by keycode
+			var code = e.keyCode
+
+			var isNumberKey = code >= 48 && code <= 57
+      var isAlphaKey = code >= 65 && code <= 65+26
+
+			// Don't detect numbers, when holding shift
+			// because they are actually typing a symbol
+			// which we capture later
+			var alphanumeric = (isAlphaKey || isNumberKey && !e.shiftKey) && String.fromCharCode(e.keyCode)
+
+			// Different symbols are accesible depending on the shift key
+			var symbol = (e.shiftKey ? this.shiftSymbols : this.symbols })[code]
+
+			symbolAlias = (this.symbolAliases)[symbol]
+
+			// Special keys that are not affected by SHIFT like ESC or SPACE
+			var special = (this.specials)[code]
+
+			// Create a flat array of shortcuts to check
+			return [].concat(
+				code,
+				alphanumeric,
+        special,
+				symbol,
+        symbolAlias
+			)
+
+			// Remove any falsey entries
+		  .filter( Boolean )
+		}
+		keyNames.specials = {
+			9: 'TAB',
+			13: 'ENTER',
+			27: 'ESC',
+			32: 'SPACE',
+			37: 'LEFT',
+			38: 'UP',
+			39: 'RIGHT',
+			40: 'DOWN',
+			8: 'BACKSPACE'
+		}
+		keyNames.symbolAliases = {
+			',': 'COMMA',
+			'/': 'SLASH',
+			'\\': ['BACKSLASH','DIVIDE'],
+			'~': 'TILDE',
+			'-': ['DASH'],
+			'+': 'PLUS',
+			'*': ['STAR','MULTIPLY','TIMES'],
+			'|': ['PIPE'],
+			'&': 'AMPERSAND',
+			'@': 'AT',
+			'!': 'EXCLAMATION',
+			'#': 'HASH',
+			'$': 'DOLLAR',
+			'%': 'PERCENT',
+			'^': ['CARET','HAT'],
+			'.': ['DOT','PERIOD'],
+			'_': 'UNDERSCORE',
+			'?': 'QUESTION',
+		}
+		keyNames.shiftSymbols = {
+			48: ")", 49: "!", 50: "@", 51: "#", 52: "$", 53: "%", 54: "^", 55: "&", 56: "*",
+			57: "(", 186: ":", 187: "+", 188: "<", 189: "_", 190: ">", 191: "?", 219: "{", 220: "|",
+			221: "}", 222: '"'
+		}
+		
+		keyNames.symbols = { 186: ";", 188: ",", 190: ".", 191: "/", 220: "\\", 219: "[", 221: "]", 222: "'"}
